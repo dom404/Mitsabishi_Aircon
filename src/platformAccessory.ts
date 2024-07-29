2a421374214024bb9bc05c61090c92ef0e3cec57
Job Doesburg <job.doesburg@gmail.com>
2024-07-29 10:11:37 +0200

    Logs

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -216,12 +216,15 @@ export class WFRACAccessory {
     let targetHeatingCoolingState = this.platform.Characteristic.TargetHeatingCoolingState.OFF;
 
     if (this.device.status.operation && this.device.status.operationMode === DeviceStatus.OPERATION_MODE_COOL) {
+      this.platform.log.info('Cooling');
       targetHeatingCoolingState = this.platform.Characteristic.TargetHeatingCoolingState.COOL;
       currentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.COOL;
     } else if (this.device.status.operation && this.device.status.operationMode === DeviceStatus.OPERATION_MODE_HEAT) {
+        this.platform.log.info('Heating');
       targetHeatingCoolingState = this.platform.Characteristic.TargetHeatingCoolingState.HEAT;
       currentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.HEAT;
     } else if (this.device.status.operationMode === DeviceStatus.OPERATION_MODE_AUTO) {
+        this.platform.log.info('Auto');
       targetHeatingCoolingState = this.platform.Characteristic.TargetHeatingCoolingState.AUTO;
       if (this.device.status.isAutoHeating) {
         currentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.HEAT;
